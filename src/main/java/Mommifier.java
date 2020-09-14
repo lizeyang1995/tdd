7c03299970e7d656c4bf5a4c8423ca80b1c61c08
lizeyang1995 <424011527@qq.com>
2020-09-14 12:13:04 +0800

    [lizeyang] feat: complete a test, should return exception when convert given a null

--- src/main/java/Mommifier.java
+++ src/main/java/Mommifier.java
@@ -1,5 +1,7 @@
 public class Mommifier {
-    public void convert(Object o) {
-
+    public void convert(String string) {
+        if (string == null) {
+            throw new IllegalArgumentException();
+        }
     }
 }
