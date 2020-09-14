95d4c13e8d671af53f9e3eb6eb7b132339d4ec39
lizeyang1995 <424011527@qq.com>
2020-09-14 12:27:01 +0800

    [lizeyang] feat: complete a test, should return original string when the number of vowels is less than 30%

--- src/main/java/Mommifier.java
+++ src/main/java/Mommifier.java
@@ -1,7 +1,18 @@
 public class Mommifier {
-    public void convert(String string) {
+    public String convert(String string) {
         if (string == null) {
             throw new IllegalArgumentException();
         }
+        float numberOfVowel = 0;
+        float stringLength = string.length();
+        for (int index = 0; index < string.length(); index++) {
+            if (string.charAt(index) == 'a' || string.charAt(index) == 'e' || string.charAt(index) == 'i' || string.charAt(index) == 'o' || string.charAt(index) == 'u') {
+                numberOfVowel++;
+            }
+        }
+        if (numberOfVowel / stringLength <= 0.3) {
+            return string;
+        }
+        return null;
     }
 }
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -6,6 +6,13 @@ public class MommifierTest {
     @Test
     void should_return_exception_when_convert_given_a_null() {
         Mommifier mommifier = new Mommifier();
-        assertThrows(IllegalArgumentException.class, () -> {mommifier.convert(null);});
+        assertThrows(IllegalArgumentException.class, () -> mommifier.convert(null));
+    }
+
+    @Test
+    void should_return_original_string_when_vowel_characters_in_the_given_string_are_less_than_or_equal_to_percent_30() {
+        Mommifier mommifier = new Mommifier();
+        String result = mommifier.convert("try");
+        assertEquals("try", result);
     }
 }
